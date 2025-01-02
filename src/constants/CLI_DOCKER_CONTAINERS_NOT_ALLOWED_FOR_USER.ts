f524dde23f34ef2028eab046c86d1c45effb6a68
Maciej Koralewski <m.koralewski.98@gmail.com>
2025-01-02 21:51:19 +0100

    feat(#14): add uniffo-traefik-local configuration for Docker containers

--- src/constants/CLI_DOCKER_CONTAINERS_NOT_ALLOWED_FOR_USER.ts
+++ src/constants/CLI_DOCKER_CONTAINERS_NOT_ALLOWED_FOR_USER.ts
@@ -4,5 +4,5 @@ import { DOCKER_CONTAINERS_DICTIONARY } from '../pre_compiled/__docker_container
 
 export const CLI_DOCKER_CONTAINERS_NOT_ALLOWED_FOR_USER: typeof DOCKER_CONTAINERS_DICTIONARY[number][] = [
     'root',
-    'uniffo-traefik',
+    'uniffo-traefik-local',
 ]
